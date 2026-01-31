c57499d4de8abba938ea9d2730e4473e09ebef02
successismycolin <spumelelebuthelezi@gmail.com>
2026-01-31 22:31:45 +0200

    Add Backdrop filter for effect

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -46,7 +48,23 @@ class HomeScreen extends StatelessWidget {
                     color: Colors.deepPurple,
                   )
                 ),
-              )
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: const BoxDecoration(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+              ),
             ],
           ),
         ),
