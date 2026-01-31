031b4a577ba0fb5a77648ea55a591c65cef619d3
successismycolin <spumelelebuthelezi@gmail.com>
2026-01-31 22:23:39 +0200

    Add another coloured circle

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,6 +36,17 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              Align(
+                alignment: const AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  )
+                ),
+              )
             ],
           ),
         ),
